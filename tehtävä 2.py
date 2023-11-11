8042e37b7bf2956a8223cd6b6141bfd36ed492a8
Jonnerop <142784165+Jonnerop@users.noreply.github.com>
2023-11-11 16:19:58 +0200

    Create tehtävä 2.py
    matikka 2, tehtävä 2 käyrät

---
+++ tehtävä 2.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(6.4 * 3, 4.8 * 3))
+
+X = np.linspace(-3 * np.pi, 3 * np.pi, 256, endpoint=True)
+C, S = np.cos(X), np.sin(X)
+
+plt.plot(X, C, label='Cosine', color='blue', linestyle='--')
+plt.plot(X, S, label='Sine', color='red', linestyle='-.')
+
+plt.legend()
+plt.show()
