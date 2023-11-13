89e3c4ce73bdd0efd3df6f22838572c00e12a13b
Jonnerop <142784165+Jonnerop@users.noreply.github.com>
2023-11-13 14:02:07 +0200

    Update tehtävä 3.py

--- tehtävä 3.py
+++ tehtävä 3.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-plt.figure(figsize=(6.4 * 3, 4.8 * 3))
+plt.figure(figsize=(6.4 * 3, 4.8))
 
 X = np.linspace(-3 * np.pi, 3 * np.pi, 256, endpoint=True)
 C, S = np.cos(X), np.sin(X)
